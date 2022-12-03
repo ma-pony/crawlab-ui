fe924b2efb9ccbe9ca55877ebd39075783f6f348
Marvin Zhang <tikazyq@163.com>
2022-12-03 16:01:45 +0800

    build: updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,25 @@ import vue from '@vitejs/plugin-vue';
 import dynamicImport from 'vite-plugin-dynamic-import';
 
 export default defineConfig({
+  build: {
+    lib: {
+      name: 'crawlab-ui',
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'crawlab-ui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+  },
   resolve: {
     alias: [
       {find: '@', replacement: resolve(__dirname, 'src')},
